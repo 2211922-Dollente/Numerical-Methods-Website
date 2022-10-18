4416c8facd8fb54b744d8f9215accff5e7fc855d
man-myth <manmeets1100@gmail.com>
2022-10-18 15:46:03 +0800

    add formula and view

--- js/script.js
+++ js/script.js
@@ -2,24 +2,47 @@
 var button = document.getElementById('calculate')
 
 button.addEventListener('click', calculate);
-// const ce = new ComputeEngine.ComputeEngine();
-// ce.numericMode = 'machine';
-
-// MathLive.renderMathInDocument()
-
+var iterations = [];
+generateAnswerTable(iterations);
 
 function calculate(){
+    $("#results").empty();
+    iterations = [];
+    document.getElementById("results").innerHTML = '';
     let fx = new Polynomial($("#function").val());
-    let x1 = $("#initial-value");
-    let error = $("#error-percent");
-
+    let x1 = $("#initial-value").val();
+    var ea = $("#error-percent").val() /100;
     let pe = 1;
     let derivative = fx.derive(1);
-
-    while(pe>error){
+    index = 0;
+    while(pe > ea){
         let x2 = x1 - (fx.eval(x1)/derivative.eval(x1));
-
+        iterations.push([index, x1,pe]);
+        pe= Math.abs((x2-x1)/x2)*100;
+        x1 = x2;
+        index ++;
+        // console.log("X:"+ x2 + " ---- " + pe + "%");
     }
 
+    generateAnswerTable(iterations);
 }
 
+function generateAnswerTable(iterations){
+    let tableHeaders = ['x<sub>i</sub>', 'f(x<sub>i-1</sub>)', 'ε<sub>a</sub>'];
+    var results = $("#results");
+    let hr = $('<tr></tr>');
+    tableHeaders.forEach(e => {
+        hr.append($(`<th>${e}</th>`));
+    });
+    results.append(hr);
+
+    for (let i = 0; i < iterations.length; i++) {
+        let tr = $('<tr class="table-data"></tr>');
+        console.log(iterations);
+        iterations[i].forEach(e => {
+            let th = $(`<td>${e}</td>`);
+            tr.append(th);
+        })
+        results.append(tr);
+    }
+}
