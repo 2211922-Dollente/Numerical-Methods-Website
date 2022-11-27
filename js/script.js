e2dfdbb0192c47aacd51459667fa0846f4736486
man-myth <manmeets1100@gmail.com>
2022-11-27 20:22:38 +0800

    allow different variables

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+var variable;
 // Add keyboards
 var mq = window.matchMedia( "(max-width: 768px)" );
 if (mq.matches) {
@@ -9,19 +10,23 @@ if (mq.matches) {
 }
 var button = document.getElementById('calculate')
 
-button.addEventListener('click', calculate);
-var iterations = [];
-generateAnswerTable(iterations);
+button.addEventListener('click', calculate);        // add event listener to calculate
+var iterations = [];                                
+// generateAnswerTable(iterations);
 
 function calculate(){
-    let steps = $("#steps-wrapper");
+    let steps = $("#steps-wrapper");                        //clear steps
     steps.empty();
 
-    let fx = new Polynomial($("#function").val());
+    // let fx = new Polynomial($("#function").val());
+    let fx = $("#function").val();
+    console.log(fx.toString());
+    console.log(fx.length);
+    let variable = findVariable(fx);            
     let x1 = $("#initial-value").val();
     var ea = $("#error-percent").val() /100;
 
-    if(fx == 0 || isNaN(x1) || ea == 0|| isNaN(ea)){ 
+    if(fx == 0 || isNaN(x1) || ea == 0|| isNaN(ea)){        //input validation
         return;
     }
     
@@ -29,8 +34,10 @@ function calculate(){
     let iterations = [];
     document.getElementById("results").innerHTML = '';
     
-    let pe = 1;                                     //Percent Error
-    let derivative = fx.derive(1);                  //get the 1st derivative
+    let pe = 1;                                         //Percent Error
+    // let derivative = fx.derive(1);                  //get the 1st derivative
+    let derivative = math.derivative(fx,variable).toString();                //get the 1st derivative
+    console.log("Derivative: " + derivative);
     index = 0;
     iterations.push([index, x1,pe]);
     index ++;
@@ -38,16 +45,19 @@ function calculate(){
     <h2>Step-by-Step Solution</h2>
     <dl>
         <dt>Problem:</dt>
-        <dd>&nbsp<math-field read-only>f(x) = ${fx}</math-field></dd>
-        <dd>&nbsp<math-field read-only>f'(x) = ${derivative}</math-field></dd>
+        <dd>&nbsp<math-field read-only>f(${variable}) = ${fx}</math-field></dd>
+        <dd>&nbsp<math-field read-only>f'(${variable}) = ${derivative}</math-field></dd>
         <dd>&nbspInitial Value = ${x1}</dd>
         <dd>&nbspPercent of error = ${ea}</dd> 
     </dl> <br>  
     `);
+    const parser = math.parser();
 
     while(pe > ea){
-        let fxValue = Math.round(((fx.eval(x1)) + Number.EPSILON) * 1000000) / 1000000;
-        let dxValue = Math.round(((derivative.eval(x1)) + Number.EPSILON) * 1000000) / 1000000;
+       
+        parser.evaluate(`${variable} = ${x1}`);
+        let fxValue = Math.round((parser.evaluate(fx) + Number.EPSILON) * 1000000) / 1000000;
+        let dxValue = Math.round((parser.evaluate(derivative) + Number.EPSILON) * 1000000) / 1000000;
         let x2 = Math.round(((x1 - (fxValue/dxValue)) + Number.EPSILON) * 1000000) / 1000000; 
         pe= Math.round(((Math.abs((x2-x1)/x2)*100) + Number.EPSILON) * 1000000) / 1000000; 
 
@@ -61,9 +71,9 @@ function calculate(){
         steps.append(`
         <dl>
             <dt>Iteration ${index}:</dt>
-            <dd>&nbsp<math-field read-only>f(x) = ${fts} = ${fxValue}</math-field></dd>
-            <dd>&nbsp<math-field read-only>f'(x) = ${fds} = ${dxValue}</math-field></dd>
-            <dd>&nbsp<math-field read-only>X \\scriptstyle{${index+1}} \\displaystyle = ${x1} - \\frac{${fxValue}}{${dxValue}}  =  ${x2}</math-field></dd>
+            <dd>&nbsp<math-field read-only>f(${variable}) = ${fts} = ${fxValue}</math-field></dd>
+            <dd>&nbsp<math-field read-only>f'(${variable}) = ${fds} = ${dxValue}</math-field></dd>
+            <dd>&nbsp<math-field read-only>${variable} \\scriptstyle{${index+1}} \\displaystyle = ${x1} - \\frac{${fxValue}}{${dxValue}}  =  ${x2}</math-field></dd>
             <dd>&nbsp<math-field read-only>ε \\scriptstyle{a} \\displaystyle = \\frac{${x2}-${x1}}{${x2}} \\times 100 = ${pe} </math-field><span>&nbsp%</span></dd> 
          </dl> <br>  
         `);
@@ -76,11 +86,31 @@ function calculate(){
     generateAnswerTable(iterations);    
     let finalResult = $("#final-result");
     finalResult.empty();
-    finalResult.append("x = " + Number(x1).toFixed(4));
+    finalResult.append(variable+ " = " + Number(x1).toFixed(4));
+}
+
+
+function findVariable(fx) {         //returns the variable used in the function
+    let alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    
+    for (i = 0; i < fx.length; i++) {
+        for (j = 0; j < alpha.length; j++) {
+            if (fx[i].toLowerCase() == alpha[j]) {
+                variable = fx[i];
+                console.log(variable);
+                break;
+            }
+        }
+        if (variable == fx[i]) {
+            console.log("break");
+            break;
+        }
+    }
+    return variable;
 }
 
 function generateAnswerTable(iterations){
-    let tableHeaders = ['x<sub>i</sub>', 'f(x<sub>i-1</sub>)', 'ε<sub>a</sub>'];
+    let tableHeaders = [`${variable}<sub>i</sub>`, `f(${variable}<sub>i-1</sub>)`, `ε<sub>a</sub>`];
     var results = $("#results");
     let hr = $('<tr></tr>');
     tableHeaders.forEach(e => {
